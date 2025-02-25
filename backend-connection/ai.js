24e8ab5a1ca702f9b0dc12de6053951dacbc95b0
Ben C. <greenboyb1@gmail.com>
2025-02-25 00:46:35 -0500

    new updates for the site

--- backend-connection/ai.js
+++ backend-connection/ai.js
@@ -1,87 +1,56 @@
+document.getElementById("usrMessage").addEventListener("keypress", (x) => {
+    if (x.key === "Enter" && !x.shiftKey) {
+        x.preventDefault();
+        sendMessage();
+    }
+});
+
 function sendMessage() {
     const userMsg = document.getElementById("usrMessage").value;
+    const userCode = document.getElementById("codeMessage").value;
     const http = new XMLHttpRequest();
-    const url = "http://localhost:3006/hello";
+    const url = "https://bencarpenterit.com:3006/questions";
 
-    http.open("GET", url, true);
+    document.getElementById("usrMessage").value = "";
+    document.getElementById("messageHistory").innerHTML =
+        document.getElementById("messageHistory").innerHTML +
+        '<div class="message"><div class="humanMessage">' +
+        userMsg +
+        "</div></div>";
+    document.getElementById("messageHistory").innerHTML =
+        document.getElementById("messageHistory").innerHTML +
+        '<div class="message"><div id="lastBotMessage" class="botMessage">' +
+        "Animations Here" +
+        "</div></div>";
+
+    document.getElementById("messageHistory").scrollTop =
+        document.getElementById("messageHistory").scrollHeight;
+
+    http.open("POST", url, true);
 
     //Send the proper header information along with the request
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.setRequestHeader("Content-type", "text/plain");
     http.setRequestHeader("userMsg", userMsg);
 
     http.onreadystatechange = function () {
         //Call a function when the state changes.
         if (http.readyState == 4 && http.status == 200) {
-            document.getElementById("usrMessage").value = "";
-            document.getElementById("messageHistory").innerHTML =
-                document.getElementById("messageHistory").innerHTML +
-                '<div class="message"><div class="humanMessage">' +
-                userMsg +
-                "</div></div>";
-            receiveMessage(this.responseText);
+            document.getElementById("lastBotMessage").innerHTML =
+                http.responseText;
+            document.getElementById("lastBotMessage").setAttribute("id", "");
+            document.getElementById("messageHistory").scrollTop =
+                document.getElementById("messageHistory").scrollHeight;
         } else if (http.readyState == 4 && http.status != 200) {
-            document.getElementById("messageHistory").innerHTML =
-                document.getElementById("messageHistory").innerHTML +
-                '<div class="message"><div class="errorMessage">' +
-                "Something Failed" +
-                "</div></div>";
+            document.getElementById("lastBotMessage").innerHTML =
+                "An error has occurred, please try again.";
+            document
+                .getElementById("lastBotMessage")
+                .setAttribute("class", "errorMessage");
+            document.getElementById("lastBotMessage").setAttribute("id", "");
+            document.getElementById("messageHistory").scrollTop =
+                document.getElementById("messageHistory").scrollHeight;
         }
     };
 
-    http.send();
-}
-
-function receiveMessage(receivedMsg) {
-    document.getElementById("messageHistory").innerHTML =
-        document.getElementById("messageHistory").innerHTML +
-        '<div class="message"><div class="botMessage">' +
-        receivedMsg +
-        "</div></div>";
-}
-
-//For reference from old code
-function sendMail() {
-    var sender = document.getElementById("sender").value;
-    var subject = document.getElementById("subject").value;
-    var body = document.getElementById("body").value;
-    var flname = document.getElementById("flname").value;
-    var resume = document.getElementById("resumeyn").checked;
-
-    var http = new XMLHttpRequest();
-
-    var url = "../mail.php";
-    var params = `?sub=${subject}&body=${body}&sender=${sender}&name=${flname}&resume=${resume}`;
-    var urlp = url + params;
-    http.open("POST", urlp, true);
-
-    //Send the proper header information along with the request
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    http.onreadystatechange = function () {
-        //Call a function when the state changes.
-        if (http.readyState == 4 && http.status == 200) {
-            document.getElementById("alerts").innerHTML =
-                '<span style="color: #519D48">Message Sent Successfully.</span>';
-            document.getElementById("subbutton").removeAttribute("enabled", "");
-            document.getElementById("subbutton").setAttribute("disabled", "");
-            setTimeout(function () {
-                document.getElementById("alerts").innerHTML = "&nbsp;";
-                document
-                    .getElementById("subbutton")
-                    .setAttribute("enabled", "");
-                document
-                    .getElementById("subbutton")
-                    .removeAttribute("disabled", "");
-            }, 20000);
-            //alert(http.responseText);
-        } else {
-            document.getElementById("alerts").innerHTML =
-                '<span style="color: #D13B3B">Failed to Send Message, Please Try again.</span>';
-            setTimeout(function () {
-                document.getElementById("alerts").innerHTML = "&nbsp;";
-            }, 15000);
-        }
-    };
-    console.log(params);
-    http.send();
+    http.send(userCode);
 }
---
+++ backend-connection/colorize.js
@@ -0,0 +1,47 @@
+const resizeTextarea = (textArea, highlightArea) => {
+    if (!textArea || !highlightArea) {
+        return;
+    }
+
+    window.requestAnimationFrame(() => {
+        console.log(textArea.offsetWidth);
+        highlightArea.style.width = `${textArea.offsetWidth - 3}px`;
+        textArea.style.height = 0;
+        highlightArea.style.fontSize = window
+            .getComputedStyle(document.getElementById("codeMessage"), null)
+            .getPropertyValue("font-size");
+        if (textArea.scrollHeight > 0) {
+            textArea.style.height = `${textArea.scrollHeight + 2}px`;
+            highlightArea.style.height = `${textArea.offsetHeight - 3}px`;
+        }
+    });
+};
+
+const highlight = (inputEl, highlightEl) => {
+    window.requestAnimationFrame(() => {
+        const highlighted = hljs.highlight(inputEl.value, {
+            language: "cpp",
+        }).value;
+        highlightEl.innerHTML = highlighted;
+    });
+};
+
+const init = (inputEl, highlightEl) => {
+    inputEl.addEventListener("input", () => {
+        resizeTextarea(inputEl, highlightEl);
+        highlight(inputEl, highlightEl);
+    });
+    window.addEventListener("resize", () => {
+        resizeTextarea(inputEl, highlightEl);
+        highlight(inputEl, highlightEl);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const inputEl = document.getElementById("codeMessage");
+    const highlightEl = document.getElementById("highlightMe");
+    init(inputEl, highlightEl);
+    resizeTextarea(inputEl, highlightEl);
+    highlight(inputEl, highlightEl);
+    highlightEl.style.font = inputEl.style.font;
+});
