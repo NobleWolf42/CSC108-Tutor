ed0b92f99863adb62d9a99585adb4768c211742f
Ben C. <greenboyb1@gmail.com>
2025-02-26 00:22:25 -0500

    server should handel code now

--- backend-connection/query.h
+++ backend-connection/query.h
@@ -4,6 +4,7 @@
 std::string queryOllama(std::string input, std::string userCode)
 {
     std::string response = ollama::generate("llama3.2", input);
-    std::cout << "Response from Ollama: " << response << std::endl;
+    std::cout << "Response from Ollama: " << response << std::endl
+              << std::endl;
     return response;
 }
