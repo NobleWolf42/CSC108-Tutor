defd02677e6c1531a0cdbbd7f653dcebdfe30c6a
Ben C. <greenboyb1@gmail.com>
2025-02-26 00:37:12 -0500

    it compiles code now

--- backend-connection/ai.js
+++ backend-connection/ai.js
@@ -56,3 +56,42 @@ function sendMessage() {
 
     http.send(userCode);
 }
+
+function runCode() {
+    const userInput = document.getElementById("usrInput").value;
+    const userCode = document.getElementById("codeMessage").value;
+    const userCodeNewLine = userCode.replace(/\r\n|\r|\n/g, "\\n");
+    const userCodeNoQuote = userCodeNewLine.replace(/"/g, '\\"');
+    const http = new XMLHttpRequest();
+    const url = "https://bencarpenterit.com:3006/code";
+
+    document.getElementById("codeOutput").innerHTML = "Animations Here";
+
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "text/plain");
+    http.setRequestHeader("userInput", userInput);
+
+    http.onreadystatechange = function () {
+        //Call a function when the state changes.
+        if (http.readyState == 4 && http.status == 200) {
+            const res = JSON.parse(http.response);
+            if (res.error == null && res.output != null) {
+                document.getElementById("codeOutput").innerHTML = res.output;
+            } else if (res.error != null) {
+                document.getElementById(
+                    "codeOutput"
+                ).innerHTML = `<span class="codeError">Error:</span> ${res.error}`;
+            } else {
+                document.getElementById("codeOutput").innerHTML =
+                    '<span class="codeError">An error has occurred reading the response, please try again.</span>';
+            }
+        } else if (http.readyState == 4 && http.status != 200) {
+            document.getElementById("codeOutput").innerHTML =
+                '<span class="codeError">An error has occurred, please try again.</span>';
+        }
+    };
+
+    http.send(userCodeNoQuote);
+}
